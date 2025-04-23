19d76622ad1304aa8e5e0a59050a67f81516f64f
adhy.chandra <adhy.chandra@gmail.com>
2025-04-23 13:18:53 +0700

    menambahkan liga metode bayar dan liga kasir

--- cms/views.py
+++ cms/views.py
@@ -3,6 +3,7 @@ from django.contrib import messages
 from pos.models import Penjualan, Barang
 from django.db.models import Q,Sum,Count
 import datetime
+from django.contrib.auth.models import User
 
 
 def bulannya(bulannya):
@@ -69,6 +70,39 @@ def index(request):
             for bar in barang:
                 list_nama_barang.append(bar.nama)
                 list_jumlah_barang.append(bar.jumlah_dibeli)
+            
+            # mengisi jumlah transaksi pengguna
+            liga_kasir = Penjualan.objects.all().filter(Q(is_paid=True) & Q(cabang=request.user.userprofile.cabang) & Q(updated_at__month=4) & Q(updated_at__year=datetime.datetime.now().year)).values('user').annotate(jumlah=Count('user'))
+            nama_kasir = []
+            jumlah_transaksi=[]
+            for kasir in liga_kasir:
+                nama_kasir.append(User.objects.get(id=int(kasir['user'])).username)
+                jumlah_transaksi.append(kasir['jumlah'])
+
+            # mengisi jumlah pembayaran cash vs transfer
+            liga_metode = Penjualan.objects.all().filter(Q(is_paid=True) & Q(cabang=request.user.userprofile.cabang) & Q(updated_at__month=4) & Q(updated_at__year=datetime.datetime.now().year)).values('metode').annotate(jumlah=Count('metode')).order_by()
+            print(liga_metode)
+            jumlah_metode = []
+            # apakah metode cash?
+            try:
+                if(liga_metode[0]['metode']==0):
+                    # tambahkan jumlah metodenya
+                    jumlah_metode.append(liga_metode[0]['jumlah'])
+                else:
+                    # jika tidak ada maka kasih angka 0
+                    jumlah_metode.append(0)
+            except:
+                jumlah_metode.append(0)
+            # apakah metode transfer?
+            try:
+                if(liga_metode[1]['metode']==1):
+                    jumlah_metode.append(liga_metode[1]['jumlah'])
+                else:
+                    jumlah_metode.append(0)
+            except:
+                jumlah_metode.append(0)
+            
+            print(jumlah_metode)
 
             context = {
                 'bulan':bulan,
@@ -90,7 +124,10 @@ def index(request):
                 'november':cekTotal(november['total']),
                 'desember':cekTotal(desember['total']),
                 'list_nama_barang':list_nama_barang,
-                'list_jumlah_barang':list_jumlah_barang
+                'list_jumlah_barang':list_jumlah_barang,
+                'nama_kasir':nama_kasir,
+                'jumlah_transaksi':jumlah_transaksi,
+                'jumlah_metode':jumlah_metode
             }
             return render(request,'administrator/index.html',context)
         else:
