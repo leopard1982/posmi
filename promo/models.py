f92aa85d07b002d071d630aad1fba7c4c5123b4b
adhy.chandra <adhy.chandra@gmail.com>
2025-05-12 17:18:10 +0700

    test dulu

--- promo/models.py
+++ promo/models.py
@@ -15,8 +15,8 @@ class Promo(models.Model):
     def __str__(self):
         return f"{self.kode} - {self.nama} - {self.is_active} - {self.end_period.strftime("%d/%m/%Y")} - {self.kuota}"
 
-class PromoUsed(models.Model):
-    promo = models.ForeignKey(Promo,on_delete=models.RESTRICT,null=True,blank=True,related_name="promo_master")
-    cabang = models.ForeignKey(Cabang,on_delete=models.RESTRICT,null=True,blank=True,related_name="cabang_used")
-    created_at = models.DateTimeField(auto_now_add=True)
+# class PromoUsed(models.Model):
+#     promo = models.ForeignKey(Promo,on_delete=models.RESTRICT,null=True,blank=True,related_name="promo_master")
+#     cabang = models.ForeignKey(Cabang,on_delete=models.RESTRICT,null=True,blank=True,related_name="cabang_used")
+#     created_at = models.DateTimeField(auto_now_add=True)
 
