1e17c6bf8ef91f2c1313acd44eee1aad95b471ef
adhy.chandra <adhy.chandra@gmail.com>
2025-05-16 07:56:54 +0700

    penambahan kondisi segmentasi voucher

--- promo/models.py
+++ promo/models.py
@@ -11,7 +11,10 @@ class Promo(models.Model):
     end_period = models.DateField(null=True,blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     kuota = models.IntegerField(default=0)
-    # is_tambah_kuota = models.BooleanField(default=False,null=True,blank=True)
+    is_tambah_kuota = models.BooleanField(default=False,null=True,blank=True)
+    is_registrasi = models.BooleanField(default=False,blank=True,null=True)
+    is_upgrade_lisensi = models.BooleanField(default=False,blank=True,null=True)
+    is_perpanjangan = models.BooleanField(default=False,blank=True,null=True)
 
     def __str__(self):
         return f"{self.kode} - {self.nama}"
