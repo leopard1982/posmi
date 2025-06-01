13b88da28a1ff2da9820b018ed1be8e62d73d56d
adhy.chandra <adhy.chandra@gmail.com>
2025-06-01 23:40:47 +0700

    scheduler update di run setiap jam 00:01

--- jobs/updater.py
+++ jobs/updater.py
@@ -3,5 +3,5 @@ from .jobs import updateKuota
 
 def start():
     scheduler = BackgroundScheduler()
-    scheduler.add_job(updateKuota,'cron',month="1-12",day="1",hour="23",minute="40")
+    scheduler.add_job(updateKuota,'cron',month="1-12",day="1",hour="0",minute="1")
     scheduler.start()
