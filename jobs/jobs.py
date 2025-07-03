47738ff824e098de320c821c5ce74155195bef12
leopard1982 <suryoadhychandra@outlook.com>
2025-07-03 14:36:32 +0700

    interval delete file di download_history

--- jobs/jobs.py
+++ jobs/jobs.py
@@ -1,4 +1,6 @@
 from stock.models import Cabang,DaftarPaket
+import os
+from django.conf import settings
 
 def updateKuota():
     cabang = Cabang.objects.all()
@@ -11,3 +13,10 @@ def updateKuota():
         except:
             pass
     print('ok, max transaksi sudah diupdate')
+
+def delHistoryDownload():
+    base_dir = settings.BASE_DIR
+    listfile = os.listdir(os.path.join(base_dir,'download_history'))
+    for file in listfile:
+        os.remove(os.path.join(base_dir,f'download_history/{file}'))
+    print('finish')
--- jobs/updater.py
+++ jobs/updater.py
@@ -1,8 +1,9 @@
 from apscheduler.schedulers.background import BackgroundScheduler
-from .jobs import updateKuota
+from .jobs import updateKuota,delHistoryDownload
 
 def start():
     scheduler = BackgroundScheduler()
     scheduler.add_job(updateKuota,'cron',month="1-12",day="1",hour="0",minute="1")
+    scheduler.add_job(delHistoryDownload,'interval',seconds=60*60*24) #hapus file download_history setiap 24 jam sekali
     scheduler.start()
     
